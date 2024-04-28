d4c5099db6d3a334c48959d0f11900722c6ef3e6
Abhishek <abhishek_srivastava1@intuit.com>
2024-04-28 14:39:09 +0530

    intial commit

---
+++ src/context/github/GithubContext.js
@@ -0,0 +1,24 @@
+import { createContext, useReducer } from "react";
+import githubReducer from "./GithubReducer";
+
+const GithubContext = createContext()
+
+export const GithubContextProvider = ({children}) => {
+    const initialState = {
+        users: [],
+        user: {},
+        repos: [],
+        loading: false
+    }
+
+    const [state, dispatch] = useReducer(githubReducer, initialState)
+
+    return <GithubContext.Provider value={{
+        ...state,
+        dispatch,
+    }}>
+        {children}
+    </GithubContext.Provider>
+}
+
+export default GithubContext;
